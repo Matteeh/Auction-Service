76369b06dccd77046e9449feda1f4b56cd1d65f8
matteeh <mathias.rahikainen@gmail.com>
2021-02-18 09:08:14 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Auction Service
+
+The main objective of this project was to get more experience with serverless and AWS.
+
+## Project Schema
+
+![Schema](./schema.jpg)
---
+++ auction-service/src/lib/closeAuction.js
@@ -0,0 +1,20 @@
+import AWS from "aws-sdk";
+
+const db = new AWS.DynamoDB.DocumentClient();
+
+export async function closeAuction(auction) {
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Key: { id: auction.id },
+    UpdateExpression: "set #status = :status",
+    ExpressionAttributeValues: {
+      ":status": "CLOSED",
+    },
+    ExpressionAttributeNames: {
+      "#status": "status",
+    },
+  };
+
+  const result = await db.update(params).promise();
+  return result;
+}
